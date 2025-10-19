81c7a7edb54b7b565ded933f9fda45f5bde8ed07
jonatan cuellar <jonatancuellar58@gmail.com>
2025-10-19 02:43:36 -0500

    modificaciones codigo estudiantes

--- src/main/java/Model/Colegio.java
+++ src/main/java/Model/Colegio.java
@@ -4,12 +4,13 @@
  */
 package Model;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Jonatan
  */
 public class Colegio {
-    
      private Profesor[] profesores;
     private Estudiante[] estudiantes;
     private int numProfesores;
@@ -25,27 +26,54 @@ public class Colegio {
     public void agregarProfesor(Profesor p) {
         if (numProfesores < profesores.length) {
             profesores[numProfesores++] = p;
+        } else {
+            System.out.println("no se pueden registrar mas profesores.");
         }
     }
 
     public void agregarEstudiante(Estudiante e) {
         if (numEstudiantes < estudiantes.length) {
             estudiantes[numEstudiantes++] = e;
+        } else {
+            System.out.println("no se pueden registrar mas estudiantes.");
         }
     }
 
-    public void mostrarProfesores() {
-        System.out.println("=== lista de profesores ===");
+    
+
+    public void mostrarProfesoresPorSalario() {
+    
+        if (numProfesores == 0) {
+            System.out.println("No hay profesores registrados.");
+            return;
+        } else {
+        }
+
+        
+        Arrays.sort(profesores, 0, numProfesores, 
+            (p1, p2) -> Double.compare(p1.calcularSalarioMensual(), p2.calcularSalarioMensual())
+        );
+
+        System.out.println("=== Profesores ordenados por salario (menor a mayor) ===");
+        double totalPrestaciones = 0;
         for (int i = 0; i < numProfesores; i++) {
             profesores[i].mostrarInfo();
+            totalPrestaciones += profesores[i].calcularPrestaciones();
         }
+        System.out.println("total de las prestaciones sociales : " + totalPrestaciones);
     }
 
     public void mostrarEstudiantes() {
+        if (numEstudiantes == 0) {
+            System.out.println("no hay estudiantes registrados.");
+            return;
+        }
+
         System.out.println("=== lista de estudiantes ===");
         for (int i = 0; i < numEstudiantes; i++) {
             estudiantes[i].mostrarInfo();
         }
+        
     }
     
 }
